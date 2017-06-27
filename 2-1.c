6272b0e4f2e607a8471f6fda15b94e00ac5ab9e0
aditiverma1127 <aditiverma1127@gmail.com>
2017-06-27 12:56:11 +0530

    Add files via upload

---
+++ 2-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include<limits.h>
+#include<float.h>
+/*determine the range*/
+int main()
+{       printf("CHAR_BIT, CHAR_MAX, UCHAR_MAX, INT_MAX, LONG_MAX, LONG_MIN, SHRT_MAX, SHRT_MIN, USHRT_MAX");
+        printf("%d %d  %d  %d  %ld %ld %d %d %d",CHAR_BIT, CHAR_MAX, UCHAR_MAX, INT_MAX, LONG_MAX, LONG_MIN, SHRT_MAX, SHRT_MIN, USHRT_MAX);
+        }
